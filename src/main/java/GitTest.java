5cb4093e763905c1a4a576686e8ab7fcd7d9b880
Explorer <Explorer@qq.com>
2021-05-08 21:52:56 +0800

    first version commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * @author HXL
+ * @date 2021/5/8 - 21:50
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
